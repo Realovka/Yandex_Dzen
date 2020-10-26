8d0857fb5fd21cfce32ac61cc5c1968dc40a32a3
realovka <realovka@mail.ru>
2020-10-26 21:28:15 +0300

    add database.properties

--- src/main/java/by/realovka/connection/HikariCPDataSource.java
+++ src/main/java/by/realovka/connection/HikariCPDataSource.java
@@ -4,39 +4,38 @@ import com.zaxxer.hikari.HikariDataSource;
 import org.springframework.stereotype.Component;
 
 import javax.sql.DataSource;
+import java.io.IOException;
+import java.io.InputStream;
 import java.sql.SQLException;
 import java.util.Properties;
 
 @Component
 public class HikariCPDataSource {
-    private static final String DB_USERNAME="postgres";
-    private static final String DB_PASSWORD="Vorobei55";
-    private static final String DB_URL ="jdbc:postgresql://localhost:5432/postgres";
-    private static final String DB_DRIVER_CLASS="org.postgresql.Driver";
 
-    private static Properties properties = null;
+    private static Properties properties = new Properties();
     private static HikariDataSource dataSource;
-    static{
-        try {
 
 
-            dataSource = new HikariDataSource();
-            dataSource.setDriverClassName(DB_DRIVER_CLASS);
+   public HikariCPDataSource() {
+       try {
+           InputStream inputStream = getClass().getClassLoader().getResourceAsStream("database.properties");
+           properties.load(inputStream);
+           dataSource = new HikariDataSource();
+           dataSource.setDriverClassName(properties.getProperty("driver.class.name"));
+           dataSource.setJdbcUrl(properties.getProperty("db.url"));
+           dataSource.setUsername(properties.getProperty("db.username"));
+           dataSource.setPassword(properties.getProperty("db.password"));
 
-            dataSource.setJdbcUrl(DB_URL);
-            dataSource.setUsername(DB_USERNAME);
-            dataSource.setPassword(DB_PASSWORD);
+           dataSource.setMinimumIdle(100);
+           dataSource.setMaximumPoolSize(2000);//The maximum number of connections, idle or busy, that can be present in the pool.
+           dataSource.setAutoCommit(false);
+           dataSource.setLoginTimeout(3);
 
-            dataSource.setMinimumIdle(100);
-            dataSource.setMaximumPoolSize(2000);//The maximum number of connections, idle or busy, that can be present in the pool.
-            dataSource.setAutoCommit(false);
-            dataSource.setLoginTimeout(3);
+       } catch (IOException | SQLException e) {
+           e.printStackTrace();
+       }
 
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-    }
-
+   }
     public static DataSource getDataSource() {
         return dataSource;
     }
